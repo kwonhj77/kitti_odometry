b358edacb36c64c7c9eaa00b40c6144c56b2bcfb
Will Haley <kwonhj77@gmail.com>
2025-02-03 13:05:52 -0500

    checkpoint

--- KittiOdomNN.py
+++ KittiOdomNN.py
@@ -1,7 +1,7 @@
 import torch
 import torch.nn as nn
 from torch import Tensor
-from ResidualBlock import ResidualBlock
+import time
 
 # Used for validation
 from torchsummary import summary
@@ -11,102 +11,59 @@ from torch.utils.data import DataLoader
 
 
 class KittiOdomNN(nn.Module):
-    def __init__(self, in_channels: int = 1, image_size: tuple = (128, 128), device=None):
+    def __init__(self, in_channels: int = 3, device=None):
         super().__init__()
 
         # Initial convolution layer
-        self.conv = nn.Conv2d(
-            in_channels=in_channels, 
-            out_channels=16, 
-            kernel_size=(3, 3), 
-            padding='same', 
-            bias=False
-        )
-        self.bn = nn.BatchNorm2d(16)
-        self.relu = nn.ReLU()
-
-        # First block of residual layers and pooling
-        self.layer1 = nn.Sequential(
-            ResidualBlock(16, 16),
-            ResidualBlock(16, 16),
-            ResidualBlock(16, 16),
-            nn.AvgPool2d(kernel_size=(2, 2), stride=(2, 2))
+        conv1 = nn.Conv2d(in_channels=in_channels, out_channels=8, kernel_size=(3,3), stride=1, padding=0)
+        conv2 = nn.Conv2d(in_channels=8, out_channels=16, kernel_size=(3,3), stride=1, padding=0)
+        conv3 = nn.Conv2d(in_channels=16, out_channels=16, kernel_size=3, stride=2, padding=1)
+        maxPool = nn.MaxPool2d(kernel_size=(2,2))
+        self.conv = nn.Sequential(
+            conv1,
+            nn.ReLU(),
+            maxPool,
+            conv2,
+            nn.ReLU(),
+            maxPool,
+            conv3,
+            nn.ReLU(),
+            maxPool,
         )
         
-        # Second block of residual layers and pooling
-        self.layer2 = nn.Sequential(
-            ResidualBlock(16, 32),
-            ResidualBlock(32, 32),
-            ResidualBlock(32, 32),
-            nn.AvgPool2d(kernel_size=(2, 2), stride=(2, 2))
-        )
-        
-        # Third block of residual layers and pooling
-        self.layer3 = nn.Sequential(
-            ResidualBlock(32, 64),
-            ResidualBlock(64, 64),
-            ResidualBlock(64, 64),
-            nn.AvgPool2d(kernel_size=(2, 2), stride=(2, 2))
-        )
-        
-        # Fourth block of residual layers and pooling
-        self.layer4 = nn.Sequential(
-            ResidualBlock(64, 128),
-            ResidualBlock(128, 128),
-            ResidualBlock(128, 128),
-            nn.AvgPool2d(kernel_size=(2, 2), stride=(2, 2))
-        )
-
         self.flatten = nn.Flatten(1)
 
-        self.final = torch.nn.GRU(
-            # input_size = int(image_size[0] / 2**4 * image_size[1] / 2**4 * 128),
-            # TODO: for some reason the formula above doesn't match the size of the 4th layer.
-            input_size = 226688,
+        self.dropout = nn.Dropout(p=0.2)
+
+        self.final = [torch.nn.GRU(
+            input_size = 28336,
             hidden_size = 8,
             device = device,
-        )
-
-
-        # Flattening and final linear layer
-        # self.dropout = nn.Dropout(p=0.2)
-        # self.linear = nn.Linear(
-        #     in_features=int(image_size[0] / 2**4 * image_size[1] / 2**4 * 128),
-        #     out_features=classes_num
-        # )
+        ) for _ in range(12)]
 
     def forward(self, x: Tensor) -> Tensor:
         x = self.conv(x)
-        x = self.bn(x)
-        x = self.relu(x)
-
-        x = self.layer1(x)
-        x = self.layer2(x)
-        x = self.layer3(x)
-        x = self.layer4(x)
-
         x = self.flatten(x)
-        # x = self.dropout(x)
-        # x = self.linear(x)
-        x = self.final(x)
+        x = self.dropout(x)
 
-        return x
+        final_outs = []
+        for module in self.final:
+            output = module(x)
+            final_outs.append(output[0][:,-1])
+        return torch.stack(final_outs, dim=1)
+
     
 if __name__ == '__main__':
     device = get_device()
-    model = KittiOdomNN(in_channels=3, image_size=(376, 1241), device=device)
+    model = KittiOdomNN(in_channels=3, image_size=(376, 1241), device=device).to(device)
 
     dataset = KittiOdomDataset(r'datasets\data_odometry_csv\00.csv')
-    dataloader = DataLoader(dataset, batch_size=239, pin_memory=True)
+    dataloader = DataLoader(dataset, batch_size=19, pin_memory=True)
 
     for batch_idx, sample in enumerate(dataloader):
         X = sample['image']
-        out = model(X)
+        out = model(X.to(device))
         print(type(out))
         break
 
-    summary(model, (3, 376, 1241))
-
-    # input = torch.randn(1, 3, 376, 1241).to(device=device)
-    # out = model(input)
-    # print(out)
+    summary(model, (3, 376, 1241), device=device)
