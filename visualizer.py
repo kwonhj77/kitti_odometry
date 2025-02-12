c458e06e6131c41ad394c0ae060b8f8e16c1e86e
Will Haley <kwonhj77@gmail.com>
2025-02-12 16:39:51 -0500

    Updated visualizer

--- visualizer.py
+++ visualizer.py
@@ -4,40 +4,62 @@ import matplotlib.pyplot as plt
 import matplotlib.colors as mcolors
 import matplotlib.cm as cm
 
-DIRECTORY = "./results/quick_stacked/"
+### EDIT ME ###
+DIRECTORY = "./results/default_stacked/"
 TRAIN_OR_TEST = "train"
 BATCH_KEYS_TO_PLOT = ["rot_l2_loss", "pos_l2_loss"]
 FRAME_KEYS_TO_PLOT = ["rot_diffs", "pos_diffs"]
-DATASET = [3,]
-EPOCHS = [1,]
+DATASET = [0,2,8,9]
+EPOCHS = [1,2]
+###############
 
 # Function to close plot when "Esc" is pressed
 def _on_key(event):
     if event.key == "escape":
-        plt.close()
+        plt.close('all')
 
 # Plots L2 losses in a dataset, with each point being the l2 loss for the batch.
-def plot_per_batch(dir_path, key, dataset, epoch=None):
+# Plots multiple lines for each epoch.
+def plot_per_batch(dir_path, key, dataset, epochs=None):
     dataset = f"0{dataset}" if dataset < 10 else str(dataset)
 
-    if epoch is None:
+    if epochs is None:
         csv_path = os.path.join(dir_path, f"{key}_dataset_{dataset}.csv")
+        dfs = pd.read_csv(csv_path)
     else:
-        csv_path = os.path.join(dir_path, f"{key}_dataset_{dataset}_epoch_{epoch}.csv")
-    df = pd.read_csv(csv_path)
+        dfs = []
+        for epoch in epochs:
+            csv_path = os.path.join(dir_path, f"{key}_dataset_{dataset}_epoch_{epoch}.csv")
+            dfs.append(pd.read_csv(csv_path))
 
-    batch_idx = df['batch_idx']
-    y = df[key]
-
-    fig, ax = plt.subplots(figsize=(10, 6))
+    cmap = cm.get_cmap('viridis', len(epochs))  # Choose a colormap
+    colors = [cmap(i) for i in range(len(epochs))]
+    
+    fig, ax = plt.subplots(figsize=(10, 6), constrained_layout=True)
     fig.canvas.mpl_connect("key_press_event", _on_key)  # Bind the key event
-    title = f"{key} for dataset {dataset}: epoch {epoch}" if epoch is not None else f"{key} for dataset {dataset}"
+
+    if epochs is None:
+        x = dfs['batch_idx']
+        y = dfs[key]
+        ax.plot(x, y)
+    else:
+        for epoch, df in enumerate(dfs):
+            epoch = epoch+1  # 1-indexed
+            x = df['batch_idx']
+            y = df[key]
+            ax.plot(x, y, label=f"epoch {epoch}", color=colors[epoch-1])
+
+    title = f"{key} for dataset {dataset}"
     fig.suptitle(title)
-    ax.plot(batch_idx, y)
     ax.set_xlabel("batch #")
     ax.set_ylabel(key)
     ax.grid(True)
+    ax.legend()
 
+    fig.savefig(os.path.join(dir_path, f"{key}_dataset_{dataset}.png"))
+    plt.close(fig)
+
+# Only generates plot for a single epoch.
 def plot_per_frame(dir_path, key, dataset, epoch=None):
     dataset = f"0{dataset}" if dataset < 10 else str(dataset)
 
@@ -57,42 +79,37 @@ def plot_per_frame(dir_path, key, dataset, epoch=None):
     cmap = cm.get_cmap('viridis')  # Choose a colormap
 
 
-    fig, ax = plt.subplots(3, 1, figsize=(10, 6))
+    fig, ax = plt.subplots(3, 1, figsize=(10, 6), constrained_layout=True)
     fig.canvas.mpl_connect("key_press_event", _on_key)  # Bind the key event
     title = f"{key} for dataset {dataset}: epoch {epoch}" if epoch is not None else f"{key} for dataset {dataset}"
     fig.suptitle(title)
 
     for i in range(y_len):
-        y = df[f"{i}_{key}"].tolist()
-        for j in range(len(x)):
-            ax[i].plot(x[j], y[j], color=cmap(norm(batch_idx[j])))
-        ax[i].set_xlabel("frame #")
-        ax[i].set_ylabel(y)
-        ax[i].set_title(y)
+        y_label = f"{i}_{key}"
+        y = df[y_label].tolist()
+        for j in range(len(x)-1):
+            ax[i].plot(x[j:j+2], y[j:j+2], color=cmap(norm(batch_idx[j])))
+        ax[i].set_ylabel(y_label)
+        ax[i].set_title(y_label)
         ax[i].grid(True)
+        if i == y_len-1:
+            ax[i].set_xlabel("frame #")
 
-
-
+    fig.savefig(os.path.join(dir_path, f"{key}_dataset_{dataset}_epoch_{epoch}.png"))
+    plt.close(fig)
 
 if __name__ == '__main__':
     base_path = os.path.join(DIRECTORY, TRAIN_OR_TEST)
     for key in BATCH_KEYS_TO_PLOT:
         for dataset in DATASET:
-            if EPOCHS is not None:
-                for epoch in EPOCHS:
-                    dir_path = os.path.join(base_path, key)
-                    plot_per_batch(dir_path, key, dataset, epoch)
-            else:
-                pass
+            dir_path = os.path.join(base_path, key)
+            plot_per_batch(dir_path, key, dataset, EPOCHS)
+                
     for key in FRAME_KEYS_TO_PLOT:
         for dataset in DATASET:
             if EPOCHS is not None:
+                dir_path = os.path.join(base_path, key)
                 for epoch in EPOCHS:
-                    dir_path = os.path.join(base_path, key)
                     plot_per_frame(dir_path, key, dataset, epoch)
             else:
-                pass
-    plt.show()
-
-    # plot_l2_losses_per_dataset()
-    # plot_l2_losses_per_batch()
+                plot_per_frame(dir_path, key, dataset, None)
